d35b31e52918793f663a363d5f46ea1653c62b0f
smookiekinskuma <smookiekinskuma@gmail.com>
2024-09-21 02:16:22 +0800

    Update Inquiries.js
    - Added incomplete inquiry/review form

--- src/pages/Inquiries.js
+++ src/pages/Inquiries.js
@@ -1,10 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 const Inquiries = () => {
+    const [Title, setTitle] = useState('');
+    const [Desc, setDesc] = useState('');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`TItle: ${Title} & Description: ${Desc}`);
+    }
     return (
         <>
         <h1>Inquiries/Reviews</h1>
@@ -16,6 +22,21 @@ const Inquiries = () => {
                 <Col><Link to ="/Event">Event</Link></Col>
             </Row>
         </Container>
+
+        <form onSubmit={handleSubmit}>
+            <label>
+                Title:
+            </label>
+            <input type="text" value={Title} onChange={(e) =>
+            setTitle(e.target.value)} />
+
+            <label>
+                Desctiption:
+            </label>
+            <input type="text" value={Desc} onChange={(e) =>
+            setDesc(e.target.value)} />
+            <button type="submit">Submit</button>
+        </form>
 
         <Outlet />
         </>
