5a41d8ef0edd09f15717f809cdf98608c29634f5
Dani F. <114934334+Yuf0ria@users.noreply.github.com>
2024-11-10 23:45:11 +0800

    feature Update;
    -Adjusted Mobile compatibility for stafff in admin page

--- src/pages/adminpages/Staffbook.js
+++ src/pages/adminpages/Staffbook.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
-import '../boxcomponents/Boxes.css';
+import '../adminpages/adminCSS/Staff.css';
 
 const Staffbook = ({ staff }) => {
     return (
     <div id = "LongBox">
         <Container>
-        <Row>
-            <Col xs={2}>
-                <img id = "Longimg" src={staff.staffimage} alt={staff.Name}/>
-            </Col>
-            <Col>
-                <h3>Name: {staff.Name}</h3>
-                <h4>Role: {staff.Role}</h4>
-                <h4>Location: {staff.address} {staff.city}</h4>
-            </Col>
-        </Row>
+            <Row>
+                <Col xs={2}>
+                    <img id = "Longimg" src={staff.staffimage} alt={staff.Name}/>
+                </Col>
+                <Col className='Text'>
+                    <h3 className='Text-CSS'>Name: {staff.Name}</h3>
+                    <h4 className='Text-CSS'>Role: {staff.Role}</h4>
+                    <h4 className='Text-CSS'>Location: {staff.address} {staff.city}</h4>
+                </Col>
+            </Row>
         </Container>
     </div>
     )
